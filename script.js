19338a82b8f1d038dc9b8ddf77e5998a4453cb02
KushTagra7 <kushtagra7126@gmail.com>
2026-08-18 22:54:58 +0530

    expenses can be seen when added, so display expenses works

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 //expenses array stores all the expenses
 let expenses=[];
-
+const expenseList=document.getElementById("expense-list");
 const expenseForm=document.getElementById("expense-form");
+
+function displayExpenses(){
+    //clears what was previously on display and then shows the current state of the list
+    expenseList.innerHTML="";
+
+    expenses.forEach(function(expense){
+        const expenseItem=document.createElement("div");
+        expenseItem.textContent = `${expense.description} - ₹${expense.amount}`;
+        expenseList.appendChild(expenseItem);
+    });
+}
+
 expenseForm.addEventListener("submit",function(event){
     //"event" has the information that is submitted in the form 
     //function → create a function
     //event → parameter containing information about the event that triggered the function
+    //default action after form submission is page reload for html but preventDefault makes sure that the pages doesnt reload after submit
     event.preventDefault();
     //getting the data from the form filled by the user
     const description=document.getElementById("description").value;
@@ -24,6 +37,7 @@ expenseForm.addEventListener("submit",function(event){
     };
     //add the expense to the array of expenses
     expenses.push(expense);
+    displayExpenses();
     
 
 });
